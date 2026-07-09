71e9e591045180e0b8bcd50704d34a60ca8a1f73
Robert Dixon <79385125+robsd@users.noreply.github.com>
2026-07-10 00:56:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ fetch("links.json")
 
     if (i !== -1) {
       const prev = ring[(i - 1 + ring.length) % ring.length];
+      const random = ring[Math.floor(Math.random() * ring.length)];
       const next = ring[(i + 1) % ring.length];
 
       document.getElementById("webring").innerHTML = `
+        <p>Part of <a href="https://robsd.github.io/webring">RD's Webring</a> :)
         <nav>
           <a href="${prev}">← Prev</a> |
+          <a href="${random}">Random</a> |
           <a href="${next}">Next →</a>
         </nav>
       `;
